782bb43e80c2363fcc979a1f96ea3075cde11b6f
kvoyant <kvoyant@gmail.com>
2019-12-01 13:26:16 +0900

    git reset Current Branch

--- app/src/main/java/com/yhkim/loginuipractice/MainActivity.kt
+++ app/src/main/java/com/yhkim/loginuipractice/MainActivity.kt
@@ -2,11 +2,19 @@ package com.yhkim.loginuipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.text = "Log in"
+
+        var num1 = 10 //var 변경가능
+        var num2:Int = 20
+
+        val PI = 3.14 // 변경불가 , 타입인퍼
     }
 }
